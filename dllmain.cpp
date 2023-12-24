785664e5db76bc00de711cd96de240a93457e7f2
killcerr <kill_cerr@outlook.com>
2023-12-24 09:46:04 +0800

    fix build on windows

--- dllmain.cpp
+++ dllmain.cpp
@@ -49,6 +49,7 @@ void foreach_dirs(const auto &entry, auto &os) {
     }
 }
 #ifdef _WIN32
+#include <windows.h>
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
     switch (ul_reason_for_call) {
         case DLL_PROCESS_ATTACH:
